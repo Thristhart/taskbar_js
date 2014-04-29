06cde37ed85de118d7e4bdc38b6ddfc669cdb811
Tom Shea <tom@shea.at>
2014-04-29 03:19:04 -0700

    Search and destroy buttons

--- taskbar.js
+++ taskbar.js
@@ -18,10 +18,42 @@ var Taskbar = {
 		if(!button) {
 			button = document.createElement("li");
 		}
+		else if(button instanceof String || typeof button == "string") {
+			var elem = document.createElement("li");
+			elem.innerHTML = button;
+			button = elem;
+		}
 		button.className += " taskbar_button"; // TODO: Check if the button has this class already
 		
 		taskbar.getElementsWithClassName("taskbar_button_list")[0].appendChild(button);
-	}
+		
+		return button;
+	},
+	
+	removeButton: function(taskbar, button) {
+		button = this.getButton(taskbar, button);
+		
+		if(!button) // can't remove what isn't there
+			return;
+		
+		taskbar.removeChild(button);
+	},
+	
+	getButton: function(taskbar, button) {
+		// if it's an element and it's a button they've already got it
+		if(button.nodeName && button.className.indexOf("taskbar_button") != 0)
+			return button;
+		
+		if(button instanceof String || typeof button == "string") {
+			var buttons = this.getTaskbarButtons(taskbar);
+			for(var i in buttons) {
+				if(buttons[i].innerHTML == button)
+					return buttons[i];
+			}
+		}
+		
+		return null; // we can't find the button!
+	},
 	
 	getTaskbarButtons: function(taskbar) {
 		return taskbar.getElementsWithClassName("taskbar_button");
