14cebb73bec3c7fec1245dd7fd99c09947a81513
Tom Shea <tom@shea.at>
2014-04-29 03:00:31 -0700

    Add and get buttons

--- taskbar.js
+++ taskbar.js
@@ -12,5 +12,20 @@ var Taskbar = {
 		taskbar_button_table.appendChild(taskbar_button_list);
 		taskbar.appendChild(taskbar_button_table);
 		return taskbar;
+	},
+	
+	addButton: function(taskbar, button) {
+		if(!button) {
+			button = document.createElement("li");
+		}
+		button.className += " taskbar_button"; // TODO: Check if the button has this class already
+		
+		taskbar.getElementsWithClassName("taskbar_button_list")[0].appendChild(button);
 	}
+	
+	getTaskbarButtons: function(taskbar) {
+		return taskbar.getElementsWithClassName("taskbar_button");
+	}
+	
+	
 }
